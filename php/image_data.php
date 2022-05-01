2ac0ef3339a7a2c57dcfbda9936200096380c9cf
Ako2003 <akifmursalov@gmail.com>
2022-05-01 12:20:32 -0700

    Added

--- php/image_data.php
+++ php/image_data.php
@@ -1,6 +1,10 @@
 <?php
     while($row = mysqli_fetch_assoc($query)){
         $output .= '
-        <img class="image" src="php/images/'. $row['file'] .'" alt="">';
+        <img id="myImg" class="image" src="php/images/'. $row['file'] .'" alt="">
+        <div id="myModal" class="modal">
+        <img class="modal-content" id="img01">
+        <div id="caption"></div>
+        </div>';
     }
 ?>
